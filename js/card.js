432f609df41eed7da953bea3d7917cd05272e08e
PlahtijVadim <46941335+PlahtijVadim@users.noreply.github.com>
2019-08-08 14:00:34 +0300

    модуляция

---
+++ js/card.js
@@ -0,0 +1,70 @@
+
+'use strict';
+
+(function () {
+  var Translation = {
+    FLAT: 'Квартира',
+    HOUSE: 'Дом',
+    BUNGALO: 'Бунгало'
+  };
+  var similarMapCardTemplate = document.querySelector('template').content;
+  var mapCardTemplate = similarMapCardTemplate.querySelector('article.map__card');
+
+  function addFeatures(advert, advertElement) {
+    var featuresContainer = advertElement.querySelector('.popup__features');
+    var featureElements = featuresContainer.querySelectorAll('li');
+
+    [].forEach.call(featureElements, function (item) {
+      featuresContainer.removeChild(item);
+    });
+
+    advert.offer.features.forEach(function (item) {
+      var li = document.createElement('li');
+      li.setAttribute('class', 'feature feature--' + item);
+      featuresContainer.appendChild(li);
+    });
+  }
+
+  function addCorrectRoomEnding(advertForRoom) {
+    var ending = '';
+    if (advertForRoom.offer.rooms % 5 !== 0 && advertForRoom.offer.rooms !== 0) {
+      ending = (advertForRoom.offer.rooms === 1) ? 'а' : 'ы';
+    }
+    return ending;
+  }
+
+  function addCorrectGuestEnding(advertForGuest) {
+    var ending = (advertForGuest.offer.guests % 10 === 1 && advertForGuest.offer.guests !== 11) === true ? 'я' : 'ей';
+    return ending;
+  }
+
+  function createOneAdvert(advert) {
+    var advertElement = mapCardTemplate.cloneNode(true);
+    var picturesContainer = advertElement.querySelector('.popup__pictures');
+    var textElements = advertElement.querySelectorAll('p');
+    var extra = picturesContainer.querySelector('li');
+    addFeatures(advert, advertElement);
+
+    advertElement.querySelector('h3').textContent = advert.offer.title;
+    advertElement.querySelector('small').textContent = advert.offer.address;
+    advertElement.querySelector('.popup__price').textContent = advert.offer.price + ' ' + String.fromCharCode(8381) + ' / ночь';
+    advertElement.querySelector('h4').textContent = Translation[advert.offer.type.toUpperCase()];
+
+    textElements[2].textContent = advert.offer.rooms + ' комнат' + addCorrectRoomEnding(advert) + ' для ' + advert.offer.guests + ' гост' + addCorrectGuestEnding(advert);
+    textElements[3].textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
+
+    textElements[4].textContent = advert.offer.description;
+    advertElement.querySelector('.popup__avatar').setAttribute('src', advert.author.avatar);
+
+    picturesContainer.removeChild(extra);
+
+    advert.offer.photos.forEach(function (item) {
+      var li = document.createElement('li');
+      li.innerHTML = '<img src=' + item + ' style="width: 40px; height: 40px; padding-right: 5px; padding-bottom: 2px;">';
+      picturesContainer.appendChild(li);
+    });
+
+    return advertElement;
+  }
+  window.createOneAdvert = createOneAdvert;
+})();
---
+++ js/map.js
@@ -0,0 +1,150 @@
+'use strict';
+
+(function () {
+  var addressCoordinates = window.elements.mapForm.querySelector('#address');
+  var width = parseInt(getComputedStyle(window.elements.mainButton).getPropertyValue('left'), 10) * 2;
+  window.elements.mainButton.setAttribute('tabindex', '0');
+
+  function disableForm(fieldsetCount) {
+    for (var i = 0; i < fieldsetCount; i++) {
+      window.elements.mapForm.querySelectorAll('fieldset')[i].setAttribute('disabled', 'disabled');
+
+    }
+  }
+  disableForm(window.constants.FIELDSET_COUNT);
+
+  // активация формы, произойдет при openMap
+  function activeForm() {
+    for (var i = 0; i < window.constants.FIELDSET_COUNT; i++) {
+      window.elements.mapForm.querySelectorAll('fieldset')[i].removeAttribute('disabled');
+    }
+  }
+
+  function addAddress(input) {
+    var left = parseInt(getComputedStyle(window.elements.mainButton).getPropertyValue('left'), 10);
+    var top = parseInt(getComputedStyle(window.elements.mainButton).getPropertyValue('top'), 10);
+    input.setAttribute('value', 'x: ' + left + ' y: ' + (top + window.constants.MAIN_PIN_HEIGHT / 2 + window.constants.MAIN_POINTER_HEIGHT));
+  }
+
+  function openMap() {
+    window.elements.cityMap.classList.remove('map--faded');
+    window.elements.mapForm.classList.remove('ad-form--disabled');
+    activeForm();
+    window.joinAd();
+    if (window.adverts) {
+      window.newPins = window.pin.getRandomStartElements(window.constants.NUMBER_OF_SHOW_PINS);
+      window.pin.showMapPins(window.newPins);
+    }
+    addAddress(addressCoordinates);
+
+    var filterBox = document.querySelector('.map__filters');
+    filterBox.addEventListener('change', window.onFiltersChange);
+  }
+
+  // событие - открытие формы при нажатии на пироженку
+  window.elements.mainButton.addEventListener('mouseup', function () {
+    if (window.elements.cityMap.getAttribute('class') === 'map map--faded') {
+      openMap();
+    }
+  });
+
+  window.elements.mainButton.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openMap);
+  });
+
+  // это событие - нажатие на любой пин
+  window.elements.mapPins.addEventListener('click', function (evt) {
+    window.showCard(evt, window.newPins);
+  });
+
+  function getCloseButton() {
+    var closeButton = window.elements.cityMap.querySelector('.popup__close');
+    closeButton.setAttribute('tabindex', '0');
+    document.addEventListener('keydown', onPopupEscPress);
+    closeButton.addEventListener('mouseup', function () {
+      closeAdvert();
+    });
+
+    closeButton.addEventListener('keydown', onPopupEnterPress);
+  }
+
+  function onPopupEscPress(evt) {
+    window.util.isEscEvent(evt, closeAdvert);
+  }
+
+  function onPopupEnterPress(evt) {
+    window.util.isEnterEvent(evt, closeAdvert);
+  }
+
+  function closeAdvert() {
+    var mapCard = window.elements.cityMap.querySelector('.map__card');
+    window.elements.cityMap.removeChild(mapCard);
+    window.elements.cityMap.querySelector('.map__pin--active').classList.remove('map__pin--active');
+    document.removeEventListener('keydown', onPopupEscPress);
+  }
+
+  window.elements.mainButton.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var topPin = window.elements.mainButton.offsetTop - shift.y;
+      var leftPin = window.elements.mainButton.offsetLeft - shift.x;
+
+      // topPin - координата верхней границы метки, поэтому вычитаю из 100 высоту метки
+      // с учетом того, что у нее translate -50% (поэтому делю на 2) и еще есть высота псевдоэлемента
+
+      if (topPin < (window.constants.LIMIT_TOP - window.constants.MAIN_PIN_HEIGHT / 2 - window.constants.MAIN_POINTER_HEIGHT)) {
+        topPin = window.constants.LIMIT_TOP - window.constants.MAIN_PIN_HEIGHT / 2 - window.constants.MAIN_POINTER_HEIGHT;
+        window.elements.mapPins.setAttribute('style', 'cursor: none');
+      } else if (topPin > window.constants.LIMIT_BOTTOM - window.constants.MAIN_PIN_HEIGHT / 2 - window.constants.MAIN_POINTER_HEIGHT) {
+        topPin = window.constants.LIMIT_BOTTOM - window.constants.MAIN_PIN_HEIGHT / 2 - window.constants.MAIN_POINTER_HEIGHT;
+        window.elements.mapPins.setAttribute('style', 'cursor: none');
+      }
+
+      if (leftPin < 0) {
+        leftPin = 0;
+      } else if (leftPin > width) {
+        leftPin = width;
+      }
+
+      window.elements.mainButton.style.top = topPin + 'px';
+      window.elements.mainButton.style.left = leftPin + 'px';
+
+      addressCoordinates.setAttribute('value', 'x: ' + leftPin + ' y: ' + (topPin + window.constants.MAIN_PIN_HEIGHT / 2 + window.constants.MAIN_POINTER_HEIGHT));
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+      window.elements.mapPins.setAttribute('style', 'cursor: auto');
+
+      window.elements.mapPins.removeEventListener('mousemove', onMouseMove);
+      window.elements.mapPins.removeEventListener('mouseup', onMouseUp);
+    }
+
+    window.elements.mapPins.addEventListener('mousemove', onMouseMove);
+    window.elements.mapPins.addEventListener('mouseup', onMouseUp);
+  });
+
+  window.map = {
+    getCloseButton: getCloseButton,
+    addAddress: addAddress,
+    closeAdvert: closeAdvert
+  };
+})();
